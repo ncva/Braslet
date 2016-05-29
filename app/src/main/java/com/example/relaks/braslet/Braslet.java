ca1e6ee52378f53b0aaaed43ec755595695a8515
Relaks <relaks1237@gmail.com>
2016-05-29 19:55:40 +0600

    first graph test

--- app/src/main/java/com/example/relaks/braslet/Braslet.java
+++ app/src/main/java/com/example/relaks/braslet/Braslet.java
@@ -1,7 +1,17 @@
 package com.example.relaks.braslet;
 
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 
 public class Braslet extends AppCompatActivity {
 
@@ -9,5 +19,41 @@ public class Braslet extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_braslet);
+        //-------------------------
+        GraphView graph = (GraphView) findViewById(R.id.view);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graph.addSeries(series);
+
+
+        //---------------------------
+        mHelloTextView=(TextView)findViewById(R.id.textView2);
+     //  mgraph =(GraphicalView)findViewById(R.id.view);
+        mText=(EditText)findViewById(R.id.editText);
+    //   mgraph.isInEditMode();
+    //    Intent mgraph = buildIntent();
+     //  startActivity(mgraph);
+
     }
+    private TextView mHelloTextView;
+    private EditText mText;
+    int i=0;
+    public void onClick(View view)
+    {
+      //  mHelloTextView=(TextView)findViewById(R.id.textView2);
+       // mHelloTextView.setText("Hi Bomj!");
+        mHelloTextView.setText(mText.getText());
+        i++;
+
+       // mHelloTextView.setText(i);
+    }
+
+
+
+
 }

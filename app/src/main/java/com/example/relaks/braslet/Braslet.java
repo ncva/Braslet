19ac9e4a720e978ad08feb26ec4670f07a088b58
Relaks <relaks1237@gmail.com>
2016-05-29 11:48:32 +0600

    Basic version

---
+++ app/src/main/java/com/example/relaks/braslet/Braslet.java
@@ -0,0 +1,13 @@
+package com.example.relaks.braslet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Braslet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_braslet);
+    }
+}

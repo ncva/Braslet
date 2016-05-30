5143e1f2ff52600ba23cedc53ce71469764526ae
Relaks <relaks1237@gmail.com>
2016-05-30 23:00:39 +0600

    123 added

---
+++ app/src/main/java/com/example/relaks/braslet/BTData.java
@@ -0,0 +1,17 @@
+package com.example.relaks.braslet;
+
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.widget.Toast;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class BTData {
+
+    private BluetoothAdapter BA;
+    void Start() {
+    }
+}
--- app/src/main/java/com/example/relaks/braslet/Braslet.java
+++ app/src/main/java/com/example/relaks/braslet/Braslet.java
@@ -1,13 +1,33 @@
 package com.example.relaks.braslet;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class Braslet extends AppCompatActivity {
+
+    private BluetoothAdapter BA;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_braslet);
+        //Bluetooth load
+        BA = BluetoothAdapter.getDefaultAdapter();
+        BA.notify();
+        //Если BT не включен
+        if (!BA.isEnabled()) {
+            //Запрос на включение
+            Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(turnOn, 0);
+            //Уведомление на короткий промежуток времени
+            Toast.makeText(getApplicationContext(),"Turned on",Toast.LENGTH_LONG).show();
+        }
+        else
+        {
+            Toast.makeText(getApplicationContext(),"Already on", Toast.LENGTH_LONG).show();
+        }
     }
 }
